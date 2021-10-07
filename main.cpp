544cd4f1ad241f1b894d37297917da12106a11e1
Ekaterina Tarapatina <tarapatina.ekaterina@visdom.tech>
2021-10-07 23:09:43 +0300

    [feat] create export key func and import key func

--- main.cpp
+++ main.cpp
@@ -2,9 +2,127 @@
 #include <windows.h>
 #include "wincrypt.h"
 #include "winerror.h"
-    
+#include "string"
 
-void sample(){
+using namespace std;
+
+BOOL GetExportedKey(
+        HCRYPTKEY hKey,
+        DWORD dwBlobType,
+        LPBYTE *ppbKeyBlob,
+        LPDWORD pdwBlobLen)
+{
+    DWORD dwBlobLength = 12;
+    *ppbKeyBlob = NULL;
+    *pdwBlobLen = 0;
+
+    // Export the public key. Here the public key is exported to a
+    // PUBLICKEYBLOB. This BLOB can be written to a file and
+    // sent to another user.
+
+    if(CryptExportKey(
+            hKey,
+            0,
+            dwBlobType,
+            0,
+            nullptr,
+            &dwBlobLength))
+    {
+        printf("Size of the BLOB for the public key determined. \n");
+    }
+    else
+    {
+        printf("Error computing BLOB length.\n");
+        return FALSE;
+    }
+
+    // Allocate memory for the pbKeyBlob.
+    if(*ppbKeyBlob = (LPBYTE)malloc(dwBlobLength))
+    {
+        printf("Memory has been allocated for the BLOB. \n");
+    }
+    else
+    {
+        printf("Out of memory. \n");
+        return FALSE;
+    }
+
+    // Do the actual exporting into the key BLOB.
+    if(CryptExportKey(
+            hKey,
+            0,
+            dwBlobType,
+            0,
+            *ppbKeyBlob,
+            &dwBlobLength))
+    {
+        printf("Contents have been written to the BLOB. \n");
+        *pdwBlobLen = dwBlobLength;
+    }
+    else
+    {
+        printf("Error exporting key.\n");
+        free(*ppbKeyBlob);
+        *ppbKeyBlob = NULL;
+
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+BOOL ImportKey(HCRYPTPROV hProv, LPBYTE pbKeyBlob, DWORD dwBlobLen)
+{
+    HCRYPTKEY hPubKey;
+
+    //---------------------------------------------------------------
+    // This code assumes that a cryptographic provider (hProv)
+    // has been acquired and that a key BLOB (pbKeyBlob) that is
+    // dwBlobLen bytes long has been acquired.
+
+    //---------------------------------------------------------------
+    // Get the public key of the user who created the digital
+    // signature and import it into the CSP by using CryptImportKey.
+    // The key to be imported is in the buffer pbKeyBlob that is
+    // dwBlobLen bytes long. This function returns a handle to the
+    // public key in hPubKey.
+
+    if(CryptImportKey(
+            hProv,
+            pbKeyBlob,
+            dwBlobLen,
+            0,
+            0,
+            &hPubKey))
+    {
+        printf("The key has been imported.\n");
+    }
+    else
+    {
+        printf("Public key import failed.\n");
+        return FALSE;
+    }
+
+    //---------------------------------------------------------------
+    // Insert code that uses the imported public key here.
+    //---------------------------------------------------------------
+
+    //---------------------------------------------------------------
+    // When you have finished using the key, you must release it.
+    if(CryptDestroyKey(hPubKey))
+    {
+        printf("The public key has been released.");
+    }
+    else
+    {
+        printf("The public key has not been released.");
+        return FALSE;
+    }
+
+    return TRUE;
+}
+
+void init(){
     // Объявление и инициализация переменных.
     HCRYPTPROV hCryptProv = 0;        // дескриптор криптопровайдера
     LPCSTR UserName = "MyKeyContainer";  // название ключевого контейнера
@@ -65,6 +183,11 @@ void sample(){
 }
 
 int main() {
-    sample();
+    //    init();
+    // логин юзера
+    // если нет ключа то оставить новый
+    // если есть, то считать
+    // оставить сообщение для юзера
+    // или считать сообщение для юзера
     return 0;
 }
